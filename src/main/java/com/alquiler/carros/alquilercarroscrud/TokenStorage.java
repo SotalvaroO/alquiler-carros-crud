e8810888632c239ed409408056c103b66b91e80d
SotalvaroO <sotalvaroo1999@gmail.com>
2023-06-12 13:18:37 -0500

    vulnerabilidad parchada

--- src/main/java/com/alquiler/carros/alquilercarroscrud/TokenStorage.java
+++ src/main/java/com/alquiler/carros/alquilercarroscrud/TokenStorage.java
@@ -1,26 +1,13 @@
 package com.alquiler.carros.alquilercarroscrud;
 
-import java.security.SecureRandom;
-
-import static org.apache.xalan.xsltc.compiler.Constants.CHARACTERS;
-
-
 public class TokenStorage {
 
-    public static String TOKEN = "";
+    public static String TOKEN = "asdasdasd";
     private static final int TOKEN_LENGTH = 60;
 
     public static void generateToken() {
-        StringBuilder token = new StringBuilder();
-        SecureRandom random = new SecureRandom();
 
-        for (int i = 0; i < TOKEN_LENGTH; i++) {
-            int randomIndex = random.nextInt(CHARACTERS.length());
-            char randomChar = CHARACTERS.charAt(randomIndex);
-            token.append(randomChar);
-        }
-
-        TOKEN = token.toString();
+        TOKEN = "";
     }
 
 }
